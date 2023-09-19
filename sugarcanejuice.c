b94b62978c64ca6410a48bc4f2407bcfd703707f
Prachi3193 <122356503+Prachi3193@users.noreply.github.com>
2023-09-19 13:06:44 +0530

    Create sugarcanejuice.c

---
+++ sugarcanejuice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int i=0,t,n;
+    scanf("%d",&t);
+    while(i<t){
+        scanf("%d",&n);
+        printf("%d\n",15*n);
+        i++;
+    }
+	return 0;
+}

da762a8542367c6a2b6aaa2bca5310f46cba7215
Prachi3193 <122356503+Prachi3193@users.noreply.github.com>
2023-09-20 13:03:59 +0530

    Create three_topics.c

---
+++ three_topics.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a,b,c,x;
+    scanf("%d%d%d%d",&a,&b,&c,&x);
+    if(x==a || x==b || x==c){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+    return 0;
+}
